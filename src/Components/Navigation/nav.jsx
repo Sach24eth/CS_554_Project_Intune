fe25d20f084e26f4069c3c891b1d11ee25e09473
Tejas <60872437+tejasraibagi26@users.noreply.github.com>
2022-04-21 12:01:57 -0400

    Responsive navbar & social login button

--- src/Components/Navigation/nav.jsx
+++ src/Components/Navigation/nav.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Discord from "../../images/Discord.svg";
 import Spotify from "../../images/Spotify.png";
 import "./nav.css";
-import { loginUrl } from "../../Services/spotify";
+import { CgMenuRight, CgClose } from "react-icons/cg";
 
 const Navbar = ({ auth, username }) => {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const toggle = () => {
+    setToggleNav((prev) => !prev);
+  };
+
+  useEffect(() => {
+    let height = "250px";
+    if (toggleNav) {
+      if (!auth) height = "150px";
+      document.getElementById("header").style.height = height;
+      document.getElementById("links").classList.add("vis");
+    } else {
+      document.getElementById("header").style.height = "75px";
+      document.getElementById("links").classList.remove("vis");
+    }
+  }, [toggleNav, auth]);
+
   return (
-    <header>
-      <nav>
-        <Link to="/" className="brand">
-          <img src={Spotify} width={32} height={32} alt="Spotify Logo" />
-          <span className="cross">X</span>
-          <img src={Discord} width={32} height={32} alt="Discord Logo" />
-        </Link>
-        <div className="links">
+    <>
+      <div className="hamburger" onClick={toggle}>
+        {toggleNav ? (
+          <CgClose size={20} style={{ color: "white" }} />
+        ) : (
+          <CgMenuRight style={{ color: "white" }} size={20} />
+        )}
+      </div>
+      <header id="header">
+        <nav>
+          <Link to="/" className="brand">
+            <img src={Spotify} width={32} height={32} alt="Spotify Logo" />
+            <span className="cross">X</span>
+            <img src={Discord} width={32} height={32} alt="Discord Logo" />
+          </Link>
+
+          {/* <div className="mobile-nav" id="mobile-nav">
           {!auth ? (
             <>
               <NavLink to="/auth/signup">Sign Up</NavLink>
@@ -31,9 +58,28 @@ const Navbar = ({ auth, username }) => {
               <NavLink to="/me">{username}</NavLink>
             </>
           )}
-        </div>
-      </nav>
-    </header>
+        </div> */}
+          <div className="links" id="links">
+            {!auth ? (
+              <>
+                <NavLink to="/auth/signup">Sign Up</NavLink>
+                <NavLink to="/auth/login" className="login">
+                  Log In
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink to="/home">Home</NavLink>
+                <NavLink to="/search">Search</NavLink>
+                <NavLink to="/library">Library</NavLink>
+                <NavLink to="/playlists">Playlists</NavLink>
+                <NavLink to="/me">{username}</NavLink>
+              </>
+            )}
+          </div>
+        </nav>
+      </header>
+    </>
   );
 };
 
