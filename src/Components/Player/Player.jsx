47275c6f0a2cff3fd1593d765ee3496661328d9a
Troy Tomasch <troyrh6@gmail.com>
2022-04-24 22:45:58 -0400

    Added control functionality
    - Added play, pause, skip, previous
    - Added song title and artwork

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -9,19 +9,26 @@ import {
   FaExpand,
   FaEllipsisV,
 } from "react-icons/fa";
-import { BsBroadcast } from "react-icons/bs";
 import axios from "axios";
 
 const Player = () => {
   const [player, setPlayer] = useState(undefined);
   const [deviceId, setDeviceId] = useState(undefined);
   const [playing, setPlaying] = useState(false);
+  const [duration, setDuration] = useState("0:00");
   const [seek, setSeek] = useState(0);
+  const [active, setActive] = useState(false);
+  const [currentSong, setCurrentSong] = useState(undefined);
+  const token = window.localStorage.getItem("access_token");
   const apiUrl = "https://api.spotify.com/v1";
   const URL_PLAY = `${apiUrl}/me/player/play?device_id=${deviceId}`;
   const URL_PAUSE = `${apiUrl}/me/player/pause?device_id=${deviceId}`;
   const URL_NEXT = `${apiUrl}/me/player/next?device_id=${deviceId}`;
   const URL_PREV = `${apiUrl}/me/player/previous?device_id=${deviceId}`;
+  const URL_RECENT_TRACKS = `${apiUrl}/me/player/recently-played`;
+  const URL_STATUS = `${apiUrl}/me/player`;
+  const URL_SONG = `${apiUrl}/tracks/`;
+  const URL_TRANSFER = `${apiUrl}/me/player`;
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -31,8 +38,6 @@ const Player = () => {
     document.body.appendChild(script);
 
     window.onSpotifyWebPlaybackSDKReady = () => {
-      const token =
-        "BQBr-ukrl2YtMWiTIHbU2yPsXeqkc8-wo9UZqFU4i--Oy6JHvnUbUhM7L_78PzftX5ZNvfIkkKRhTcK6D7c5IABXXS0Xphbi6xr1drfcBO-1a0oP7na8hdzwM0DxdMdsNgun-uzBhRfqtE4RMDeoxod7Z-XHo6si1j4";
       const player = new window.Spotify.Player({
         name: "Spotify x Discord",
         getOAuthToken: (cb) => {
@@ -52,6 +57,8 @@ const Player = () => {
       player.addListener("ready", ({ device_id }) => {
         setDeviceId(device_id);
       });
+
+      getData();
     };
   }, []);
 
@@ -59,48 +66,106 @@ const Player = () => {
     setSeek((prev) => e.target.value);
   };
 
-  const playSong = async () => {
-    const userToken = window.localStorage.getItem("access_token");
-    const token =
-      "BQD30BoUkOEQVAkOs-IZAe29DV5nkFoCXvdeUyRRREYOXiAjy-3p2su10bz_AjTadMxaUUT5avaNDDkBMOZLVkSkehSFhlmsL2ePd2sWd4oKPFb8fLKAJ5fjewHK1DDrf8chCPjlULlaHalUK8nfvTeCehI";
-    let data = null;
+  const convertToTime = (millis) => {
+    let seconds = millis / 1000;
+    let minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds - minutes * 60);
+    return `${minutes}:${seconds}`;
+  };
+
+  const getData = async () => {
     try {
-      data = await axios({
+      let { data } = await axios({
+        method: "GET",
+        url: URL_STATUS,
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+      if (data) {
+        setDuration(convertToTime(data.item.duration_ms));
+        const url = URL_SONG + data.item.id;
+        try {
+          let song = await axios({
+            method: "GET",
+            url: url,
+            headers: {
+              Authorization: "Bearer " + token,
+              "Content-Type": "application/json",
+            },
+          });
+          setCurrentSong(song.data);
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        try {
+          let { data } = await axios({
+            method: "GET",
+            url: URL_RECENT_TRACKS,
+            headers: {
+              Authorization: "Bearer " + token,
+              "Content-Type": "application/json",
+            },
+          });
+          setCurrentSong(data.items[0].track);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const transferPlayback = async () => {
+    try {
+      await axios({
         method: "PUT",
-        url: URL_PLAY,
-        body: {
-          context_uri: "spotify:track:23Ij3xEFmYkRl00dDJCVMP",
-          offset: {
-            position: 0,
-          },
-          position_ms: 0,
+        url: URL_TRANSFER,
+        data: {
+          device_ids: [deviceId],
+          play: true,
         },
         headers: {
           Authorization: "Bearer " + token,
           "Content-Type": "application/json",
         },
       });
+      setActive(true);
     } catch (e) {
       console.log(e);
     }
+  };
+
+  const playSong = async () => {
+    if (!active) {
+      await transferPlayback();
+    } else {
+      try {
+        await axios({
+          method: "PUT",
+          url: URL_PLAY,
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
     setPlaying(true);
+    getData();
   };
 
   const pauseSong = async () => {
-    const token =
-      "BQD30BoUkOEQVAkOs-IZAe29DV5nkFoCXvdeUyRRREYOXiAjy-3p2su10bz_AjTadMxaUUT5avaNDDkBMOZLVkSkehSFhlmsL2ePd2sWd4oKPFb8fLKAJ5fjewHK1DDrf8chCPjlULlaHalUK8nfvTeCehI";
-    let data = null;
     try {
-      data = await axios({
+      await axios({
         method: "PUT",
         url: URL_PAUSE,
-        body: {
-          context_uri: "spotify:track:23Ij3xEFmYkRl00dDJCVMP",
-          offset: {
-            position: 0,
-          },
-          position_ms: 0,
-        },
         headers: {
           Authorization: "Bearer " + token,
           "Content-Type": "application/json",
@@ -113,11 +178,8 @@ const Player = () => {
   };
 
   const nextSong = async () => {
-    const token =
-      "BQD30BoUkOEQVAkOs-IZAe29DV5nkFoCXvdeUyRRREYOXiAjy-3p2su10bz_AjTadMxaUUT5avaNDDkBMOZLVkSkehSFhlmsL2ePd2sWd4oKPFb8fLKAJ5fjewHK1DDrf8chCPjlULlaHalUK8nfvTeCehI";
-    let data = null;
     try {
-      data = await axios({
+      await axios({
         method: "POST",
         url: URL_NEXT,
         headers: {
@@ -128,14 +190,13 @@ const Player = () => {
     } catch (e) {
       console.log(e);
     }
+    setPlaying(true);
+    getData();
   };
 
   const prevSong = async () => {
-    const token =
-      "BQD30BoUkOEQVAkOs-IZAe29DV5nkFoCXvdeUyRRREYOXiAjy-3p2su10bz_AjTadMxaUUT5avaNDDkBMOZLVkSkehSFhlmsL2ePd2sWd4oKPFb8fLKAJ5fjewHK1DDrf8chCPjlULlaHalUK8nfvTeCehI";
-    let data = null;
     try {
-      data = await axios({
+      await axios({
         method: "POST",
         url: URL_PREV,
         headers: {
@@ -146,65 +207,65 @@ const Player = () => {
     } catch (e) {
       console.log(e);
     }
+    setPlaying(true);
+    getData();
   };
 
   const toggleFullscreen = async () => {};
 
-  if (player) {
-    console.log(deviceId);
+  const writeArtists = (artists) => {
+    let list = "";
+    for (let i = 0; i < artists.length; i++) {
+      list = list + artists[i].name;
+      if (i != artists.length - 1) {
+        list = list + ", ";
+      }
+    }
+    return list;
+  };
+
+  if (currentSong) {
     return (
       <>
         <div className="bottom-player">
           <div className="track-img">
-            <img
-              src="https://i.scdn.co/image/ab67616d00001e02c50ee26def224e163f54ae0c"
-              alt="track"
-            />{" "}
-            <FaHeart className="heart icon" />
+            <img src={currentSong.album.images[0].url} alt="track" />{" "}
             <div className="track-name">
-              <p className="song">Hurricane (Arcano Remix)</p>
-              <p className="artistName">
-                Cheat Codes, Grey, Tyson Ritter, Arcando
-              </p>
+              <p className="song">{currentSong.name}</p>
+              <p className="artistName">{writeArtists(currentSong.artists)}</p>
             </div>
-            <div className="controls">
-              <div className="icons">
-                <FaBackward
-                  className="icon"
+            <FaHeart className="heart icon" />
+          </div>
+          <div className="controls">
+            <div className="icons">
+              <FaBackward
+                className="icon"
+                onClick={() => {
+                  prevSong();
+                }}
+              />
+              {!playing && (
+                <FaPlay
+                  className="white icon"
                   onClick={() => {
-                    prevSong();
+                    playSong();
                   }}
                 />
-                {!playing && (
-                  <FaPlay
-                    className="white icon"
-                    onClick={() => {
-                      playSong();
-                    }}
-                  />
-                )}
-                {playing && (
-                  <FaPause
-                    className="white icon"
-                    onClick={() => {
-                      pauseSong();
-                    }}
-                  />
-                )}
-                <FaForward
-                  className="icon"
+              )}
+              {playing && (
+                <FaPause
+                  className="white icon"
                   onClick={() => {
-                    nextSong();
+                    pauseSong();
                   }}
                 />
-              </div>
-              <div className="slider">
-                <p className="time-playing font-sm">0:54</p>
-                <p className="slider-control">
-                  <span className="slider-actual-pointer"></span>
-                </p>
-                <p className="time-total font-sm">4:00</p>
-              </div>
+              )}
+              <FaForward
+                className="icon"
+                onClick={() => {
+                  nextSong();
+                }}
+              />
             </div>
             <div className="slider">
               <p className="time-playing font-sm">0:54</p>
@@ -219,7 +280,7 @@ const Player = () => {
                 />
                 {/* <span className="slider-actual-pointer"></span> */}
               </p>
-              <p className="time-total font-sm">4:00</p>
+              <p className="time-total font-sm">{duration}</p>
             </div>
           </div>
           <div className="volume">
@@ -230,13 +291,13 @@ const Player = () => {
                 toggleFullscreen();
               }}
             />
-            <FaEllipsisV className="icon" />
-            <BsBroadcast className="icon" />
           </div>
         </div>
         ;
       </>
     );
+  } else {
+    return <h1>Could not load player</h1>;
   }
 };
 
