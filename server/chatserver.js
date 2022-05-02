c4bcdd2bcc01b96f5c73354f82c54f22c9c72af1
Sach24eth <sach24eth@gmail.com>
2022-05-02 14:51:56 -0400

    added the chat server

---
+++ server/chatserver.js
@@ -0,0 +1,28 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+var io = require("socket.io")(http);
+//console.log(io);
+app.use(cors());
+io.on("connection", (socket) => {
+  console.log("new client connected", socket.id);
+
+  socket.on("user_join", ({ name, room }) => {
+    socket.join(room);
+    io.to(room).emit("user_join", { name, room });
+  });
+
+  socket.on("message", ({ name, message, room }) => {
+    console.log(name, message, socket.id);
+    io.to(room).emit("message", { name, message });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnect Fired");
+  });
+});
+
+http.listen(4000, () => {
+  console.log(`listening on *:${4000}`);
+});
--- src/Components/Chatrooms/Chatrooms.jsx
+++ src/Components/Chatrooms/Chatrooms.jsx
@@ -134,52 +134,26 @@ function ChatroomMaker() {
         <h1>Current room: {room}</h1>
         {state.name && (
             <div className="card">
-            <div className="render-chat">
-            <h1>Chat Log</h1>
-            {renderChat()}
-            </div>
-                <form onSubmit={onMessageSubmit}>
-                <h1>Messenger</h1>
-            <div>
-              <input
-                name="message"
-                id="message"
-                variant="outlined"
-                label="Message"
-              />
-            </div>
-            <button>Send Message</button>
-          </form>
+              <div className="render-chat">
+                <h1>Chat Log</h1>
+                {renderChat()}
+              </div >
+                <form className= "messageBox" onSubmit={onMessageSubmit}>
+                  <div>
+                    <input
+                      name="message"
+                      placeholder="Say 'hi!'"
+                      id="message"
+                      variant="outlined"
+                      label="Message"
+                    />
+                  </div>
+                <button>Send Message</button>
+                </form>
+              </div>
+          )}
         </div>
-      )}
-
-      {/* {!state.name && (
-        <form
-          className="form"
-          onSubmit={(e) => {
-            //console.log(document.getElementById("username_input").value);
-            e.preventDefault();
-            setState({ name: document.getElementById("username_input").value });
-            userjoin(document.getElementById("username_input").value);
-            // userName.value = '';
-          }}
-        >
-          <div className="form-group">
-            <label>
-              User Name:
-              <br />
-              <input id="username_input" />
-            </label>
-          </div>
-          <br />
-
-          <br />
-          <br />
-          <button type="submit"> Click to join</button>
-        </form>
-      )} */}
-    </div>
-  );
+      );
 }
 
 export default ChatroomMaker;
